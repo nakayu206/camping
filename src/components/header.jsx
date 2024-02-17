88b879d673cd4edaf9965955472f49d93bc99976
nakayu206 <kinntarakame@gmail.com>
2024-02-17 15:49:09 +0900

    second

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,21 +23,33 @@ export default function Header() {
         };
     }, []);
 
+    const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            window.scrollTo({
+                top: section.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    };
+
     return (
         <header
             className={`text-gray-900 fixed top-0 left-0 right-0 z-50 transition-colors duration-500 ${
-                scrollPosition > windowHeight * 0.95 ? "bg-green-500" : "bg-white" // 背景色を切り替え
+                scrollPosition > windowHeight * 0.95 ? "bg-green-500" : ""
             }`}
         >
             <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
-                <a href="#" className="font-medium text-gray-900 mb-4 md:mb-0">
-                    <span><img src="./img/tento.png" className="w-24 md:w-40 h-auto"></img></span>
-                </a>
+                <div className="bg-green-500 p-1 border-4 mb-4 md:mb-0 md:mr-4">
+                    <a href="#" className="block">
+                        <img src="./img/tento.png" alt="Logo" className="w-24 md:w-40 h-auto border-4"></img>
+                    </a>
+                </div>
                 <nav className="md:ml-auto text-base">
-                    <a href="#saiko" onClick={() => scrollToSection("saiko")} className="mr-5 hover:text-white duration-300">
+                    <a href="#saiko" onClick={() => scrollToSection("saiko")} className="mr-5 hover:text-white duration-300 bg-green-500 px-3 py-1 rounded-lg">
                         西湖湖畔キャンプ
                     </a>
-                    <a href="#krosaka" onClick={() => scrollToSection("kurosaka")} className="mr-5 hover:text-white duration-300">
+                    <a href="#krosaka" onClick={() => scrollToSection("kurosaka")} className=" hover:text-white duration-300 bg-green-500 px-3 py-1 rounded-lg">
                         黒坂オートキャンプ
                     </a>
                 </nav>
